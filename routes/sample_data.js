911a2d08d6c763c97c47bc6350a0288667d970d7
Lucas Larsson <lucas.larsson93@hotmail.com>
2023-01-20 16:28:38 +0100

    Demo

---
+++ routes/sample_data.js
@@ -0,0 +1,64 @@
+
+var express = require('express');
+
+var router = express.Router();
+
+var database = require('../database');
+
+router.get("/", function(request, response, next){
+
+	var query = "SELECT * FROM sample_data ORDER BY id ASC";
+
+	database.query(query, function(error, data){
+
+		if(error)
+		{
+			throw error; 
+		}
+		else
+		{
+			response.render('sample_data', {title:'Kenneth & Lucas CRUD application', action:'list', sampleData:data});
+		}
+
+	});
+
+});
+
+router.get("/add", function(request, response, next){
+
+	response.render("sample_data", {title:'Register account with us', action:'add'});
+
+});
+
+router.post("/add_sample_data", function(request, response, next){
+
+	var first_name = request.body.first_name;
+
+	var last_name = request.body.last_name;
+
+	var email = request.body.email;
+
+	var password = request.body.password;
+
+	var query = `
+	INSERT INTO sample_data 
+	(first_name, last_name, password, email) 
+	VALUES ("${first_name}", "${last_name}", "${password}", "${email}")
+	`;
+
+	database.query(query, function(error, data){
+
+		if(error)
+		{
+			throw error;
+		}	
+		else
+		{
+			response.redirect("/sample_data");
+		}
+
+	});
+
+});
+
+module.exports = router;
